e21f17942050faf93ea348bc97fe39ee6d274d48
SHUBHAM KUMAR SINGH <90411262+shubham691438@users.noreply.github.com>
2024-11-12 19:57:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import KanbanBoard from './components/KanbanBoard';
+// import KanbanBoard from './components/KanbanBoard'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <KanbanBoard/>
+      </div>
+    </>
+  )
+}
+
+export default App
